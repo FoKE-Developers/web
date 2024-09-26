357f800e0e8a1c3598cda025b067680298a7cea8
Inkyu Oh <gomjellie@gmail.com>
2024-09-26 23:38:14 +0900

    feat: /s3 라우터 기본적인 기능 구현

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
-import { postRouter } from "~/server/api/routers/post";
-import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { postRouter } from '~/server/api/routers/post';
+import { s3Router } from '~/server/api/routers/s3';
+import { createCallerFactory, createTRPCRouter } from '~/server/api/trpc';
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  s3: s3Router,
 });
 
 // export type definition of API
