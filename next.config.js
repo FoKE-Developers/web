c6fa278fc041c83f20f7d7838791ce33ce86a4c5
Inkyu Oh <gomjellie@gmail.com>
2024-10-06 16:19:45 +0900

    feat: kr.object.ncloudstorage.com 허용
    - next에서 이미지 사용하기 위해서 필요

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -14,10 +14,17 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
-  transpilePackages: ["geist"],
+  transpilePackages: ['geist'],
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'kr.object.ncloudstorage.com',
+      },
+    ],
+  },
 };
 
 export default config;
