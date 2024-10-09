28163b1446a05ff3a7e9fd22f3addeeacad2166e
Inkyu Oh <gomjellie@gmail.com>
2024-10-09 18:03:38 +0900

    feat: og 태그 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,15 @@ export default function AboutPage() {
     <>
       <Head>
         <title>같이네컷</title>
-        <meta name="description" content="같이네컷" />
+        <meta name="description" content="우리 같이 네컷 사진 찍을래요?" />
+        <meta property="og:title" content="같이네컷" />
+        <meta
+          property="og:description"
+          content="우리 같이 네컷 사진 찍을래요?"
+        />
+        <meta property="og:url" content="https://4cuts.store" />
+        <meta property="og:locale" content="ko-KR" />
+        <meta property="og:image" content="/og-image.jpeg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <StyledIframe src="https://www.notioniframe.com/notion/7did5nhyric"></StyledIframe>
